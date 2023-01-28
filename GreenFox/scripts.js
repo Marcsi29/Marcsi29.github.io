2959a1f18b727bd0d052726baf0536b80332ed16
Marcsi29 <123487871+Marcsi29@users.noreply.github.com>
2023-01-28 19:58:35 +0100

    Update scripts.js

--- GreenFox/scripts.js
+++ GreenFox/scripts.js
@@ -2,3 +2,9 @@ let nevem ="Marcsi"
 let azEnKorom = 47
 console.log('A JavaScript is betoltott')
 console.log(nevem)
+
+if (azEnKorom > 18) {
+    console.log('Mar eleg idos ahhoz, hogy igyon egy sort')
+}
+
+console.log('Ez itt a vege')
