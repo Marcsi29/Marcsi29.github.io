9e7c2c7c45cc0dfce030ceaadeab29c7170d607d
Marcsi29 <123487871+Marcsi29@users.noreply.github.com>
2023-01-29 20:47:23 +0100

    szindobozok

--- GreenFox/scripts.js
+++ GreenFox/scripts.js
@@ -28,6 +28,20 @@ $('#torlo').click(() => {
     $("li:last-of-type").remove()
     })
 
-    for (let i=0; i<100; i=i+1) {
- $("ul").append('<li>${i}. Nem csalok tobbet a vizsgan.</li>')
+    for (let i = 0; i < 100; i = i + 1) {
+ $("ul").append(`<li>${i}. Nem csalok tobbet a vizsgan.</li>`)
    }
+
+   let szinek = ['orange', 'magenta', 'red', 'brown', 'blue', 'teal']
+
+   function egySzinDoboz(szin) {
+$('.kontener').append('<div class="doboz"></div>')
+$('.kontener div:last-of-type').css('background-color', szin)
+   }
+
+egySzinDoboz('lime')
+egySzinDoboz('pink')
+
+
+
+   szinek.forEach(egySzinDoboz)
